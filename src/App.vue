537de8148ed22dfae0d2849bda3d29f005ce2fb7
mrcodev <45002629+mrcodev@users.noreply.github.com>
2018-12-06 13:48:22 +0330

    first cmt

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo2.svg" id="homeLogo">
+    <HelloWorld msg="Welcome to drcoder"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+  #homeLogo {
+    width: 100px;
+    height: auto;
+  }
+</style>
